a5815887d3e0af5be099505d34c4b4a7cf49d924
kyoh-dev <contact@kyoh.run>
2022-02-18 07:39:11 +1100

    Add function skeletons

--- api/internal/models/park.go
+++ api/internal/models/park.go
@@ -1,15 +1,27 @@
 package models
 
-import "github.com/paulmach/orb/geojson"
+import (
+	"database/sql"
+	"errors"
+	"github.com/paulmach/orb/geojson"
+)
 
-type Park struct {
-	ID int `json:"id"`
-	Name string `json:"name"`
-	Type string `json:"type"`
-	AreaSqm float64 `json:"areaSqm"`
-	Hectares float64 `json:"hectares"`
-	ManagedBy string `json:"managedBy"`
-	IucnCode string `json:"iucnCode"`
-	EstablishedDate string `json:"establishedDate"`
-	Geometry geojson.MultiPolygon `json:"geometry"`
+type park struct {
+	ID              int                  `json:"id"`
+	Name            string               `json:"name"`
+	Type            string               `json:"type"`
+	AreaSqm         float64              `json:"areaSqm"`
+	Hectares        float64              `json:"hectares"`
+	ManagedBy       string               `json:"managedBy"`
+	IucnCode        string               `json:"iucnCode"`
+	EstablishedDate string               `json:"establishedDate"`
+	Geometry        geojson.MultiPolygon `json:"geometry"`
+}
+
+func (p *park) getPark(db *sql.DB) error {
+	return errors.New("not implemented")
+}
+
+func getParks(db *sql.DB, start, count int) ([]park, error) {
+	return nil, errors.New("not implemented")
 }
--- api/internal/models/rec_site.go
+++ api/internal/models/rec_site.go
@@ -1,55 +1,65 @@
 package models
 
 import (
+	"database/sql"
+	"errors"
 	"github.com/paulmach/orb/geojson"
 )
 
-type RecSite struct {
-	ID int `json:"id"`
-	Name string `json:"name"`
-	Latitude float64 `json:"latitude"`
-	Longitude float64 `json:"longitude"`
-	DisabledAccess string `json:"disabledAccess"`
-	AccessDescr string `json:"accessDescr"`
-	Fee int `json:"fee"`
-	Comments string `json:"comments"`
-	MaintainedBy string `json:"maintainedBy"`
-	ClosedStatus string `json:"closedStatus"`
-	ClosedOn string `json:"closedOn"`
-	ReopenOn string `json:"reopenOn"`
-	ClosedReason string `json:"closedReason"`
-	Camping bool `json:"camping"`
-	CampingDescr string `json:"campingDescr"`
-	Campervanning bool `json:"campervanning"`
-	CampervanningDescr string `json:"campervanningDescr"`
-	CampervanType string `json:"campervanType"`
-	TrailBikeArea bool `json:"trailBikeArea"`
-	TrailBikeAreaDescr string `json:"trailBikeAreaDescr"`
-	Heritage bool `json:"heritage"`
-	HeritageDescr string `json:"heritageDescr"`
-	Fishing bool `json:"fishing"`
-	FishingDescr string `json:"fishingDescr"`
-	Fossicking bool `json:"fossicking"`
-	FossickingDescr string `json:"fossickingDescr"`
-	HangGliding bool `json:"hangGliding"`
-	HangGlidingDescr string `json:"hangGlidingDescr"`
-	HorseRiding bool `json:"horseRiding"`
-	HorseRidingDescr string `json:"horseRidingDescr"`
-	Paddling bool `json:"paddling"`
-	PaddlingDescr string `json:"paddlingDescr"`
-	Picnicing bool `json:"picnicing"`
-	PicnicingDescr string `json:"picnicingDescr"`
-	NoiseWarning bool `json:"noiseWarning"`
-	NoiseWarningDescr string `json:"noiseWarningDescr"`
-	RockClimbing bool `json:"rockClimbing"`
-	RockClimbingDescr string `json:"rockClimbingDescr"`
-	DogWalking bool `json:"dogWalking"`
-	DogWalkingDescr string `json:"dogWalkingDescr"`
-	Wildlife bool `json:"wildlife"`
-	WildlifeDescr string `json:"wildlifeDescr"`
-	NumBbqElectric int `json:"numBbqElectric"`
-	NumBbqPit int `json:"numBbqPit"`
-	NumBbqGas int `json:"numBbqGas"`
-	NumBbqWood int `json:"numBbqWood"`
-	RepPoint geojson.Point `json:"repPoint"`
+type recsite struct {
+	ID                 int           `json:"id"`
+	Name               string        `json:"name"`
+	Latitude           float64       `json:"latitude"`
+	Longitude          float64       `json:"longitude"`
+	DisabledAccess     string        `json:"disabledAccess"`
+	AccessDescr        string        `json:"accessDescr"`
+	Fee                int           `json:"fee"`
+	Comments           string        `json:"comments"`
+	MaintainedBy       string        `json:"maintainedBy"`
+	ClosedStatus       string        `json:"closedStatus"`
+	ClosedOn           string        `json:"closedOn"`
+	ReopenOn           string        `json:"reopenOn"`
+	ClosedReason       string        `json:"closedReason"`
+	Camping            bool          `json:"camping"`
+	CampingDescr       string        `json:"campingDescr"`
+	Campervanning      bool          `json:"campervanning"`
+	CampervanningDescr string        `json:"campervanningDescr"`
+	CampervanType      string        `json:"campervanType"`
+	TrailBikeArea      bool          `json:"trailBikeArea"`
+	TrailBikeAreaDescr string        `json:"trailBikeAreaDescr"`
+	Heritage           bool          `json:"heritage"`
+	HeritageDescr      string        `json:"heritageDescr"`
+	Fishing            bool          `json:"fishing"`
+	FishingDescr       string        `json:"fishingDescr"`
+	Fossicking         bool          `json:"fossicking"`
+	FossickingDescr    string        `json:"fossickingDescr"`
+	HangGliding        bool          `json:"hangGliding"`
+	HangGlidingDescr   string        `json:"hangGlidingDescr"`
+	HorseRiding        bool          `json:"horseRiding"`
+	HorseRidingDescr   string        `json:"horseRidingDescr"`
+	Paddling           bool          `json:"paddling"`
+	PaddlingDescr      string        `json:"paddlingDescr"`
+	Picnicing          bool          `json:"picnicing"`
+	PicnicingDescr     string        `json:"picnicingDescr"`
+	NoiseWarning       bool          `json:"noiseWarning"`
+	NoiseWarningDescr  string        `json:"noiseWarningDescr"`
+	RockClimbing       bool          `json:"rockClimbing"`
+	RockClimbingDescr  string        `json:"rockClimbingDescr"`
+	DogWalking         bool          `json:"dogWalking"`
+	DogWalkingDescr    string        `json:"dogWalkingDescr"`
+	Wildlife           bool          `json:"wildlife"`
+	WildlifeDescr      string        `json:"wildlifeDescr"`
+	NumBbqElectric     int           `json:"numBbqElectric"`
+	NumBbqPit          int           `json:"numBbqPit"`
+	NumBbqGas          int           `json:"numBbqGas"`
+	NumBbqWood         int           `json:"numBbqWood"`
+	RepPoint           geojson.Point `json:"repPoint"`
+}
+
+func (r *recsite) getRecSite(db *sql.DB) error {
+	return errors.New("not implemented")
+}
+
+func getRecSites(db *sql.DB, start, count int) ([]recsite, error) {
+	return nil, errors.New("not implemented")
 }
