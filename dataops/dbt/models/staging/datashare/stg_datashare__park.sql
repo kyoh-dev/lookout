e43c55ef0c46c590a08b783cd9c801710e34b35f
Connor Keogh <contact@kyoh.run>
2022-04-19 16:18:32 +1000

    feature: Add staging model for datashare.park

--- dataops/dbt/models/staging/datashare/stg_datashare__park.sql
+++ dataops/dbt/models/staging/datashare/stg_datashare__park.sql
@@ -1,1 +1,17 @@
+select
+  objectid as id,
+  name as site_name,
+  name_short as site_name_short,
+  area_type as type,
+  total_area,
+  manager as site_manager,
+  veac_study,
+  iucn as iucn_code,
+  estab_date as established_at,
+  last_mod as modified_at,
+  vers_date as versioned_at,
+  areasqm as area_sqm,
+  hectares,
+  shape as geometry
 
+from {{ source('datashare', 'park') }}
--- dataops/migrations/000_init.sql
+++ dataops/migrations/000_init.sql
@@ -2,8 +2,6 @@
 SET client_encoding = 'UTF8';
 
 CREATE EXTENSION postgis;
-CREATE SCHEMA layers;
 
 --migrate:down
-DROP SCHEMA layers;
 DROP EXTENSION postgis;
