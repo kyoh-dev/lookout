502996a488686ebb364ff76a31176541f13ba4a1
Connor Keogh <contact@kyoh.run>
2022-04-22 16:02:55 +1000

    feature: Adjust park model and add rec_site stg

--- dataops/dbt/models/staging/datashare/stg_datashare__rec_site.sql
+++ dataops/dbt/models/staging/datashare/stg_datashare__rec_site.sql
@@ -1,1 +1,18 @@
+select
+  vers_date as versioned_at,
+  name,
+  latitude,
+  longitude,
+  dis_access as disabled_access,
+  access_dsc as directions,
+  case when fee is null
+    then 0
+    else fee::int
+  end as fee,
+  maintained_by,
+  clos_stat as closure_status,
+  clos_desc as closure_descr,
+  clos_reas as closure_reason,
+  shape as geometry
 
+from {{ source('datashare', 'datashare__rec_site') }}
